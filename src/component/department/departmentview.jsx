f3c41d6fe46ee7f2abc17de47f7217407f0b309e
hunain <ahunain99@gmail.com>
2020-01-29 15:24:26 +0530

    second commit

--- src/component/department/departmentview.jsx
+++ src/component/department/departmentview.jsx
@@ -2,39 +2,41 @@ import React, { Component } from 'react';
 import Department from './department';
 import Navbar from '../Navbar/Navbar';
 class departmentview extends Component {
-    
+
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             list:[],
+            list: [],
 
         }
     }
-    
 
-	componentDidMount() {
-		fetch('http://localhost:5000/department/',{
-            method:'GET'
+
+    componentDidMount() {
+        fetch('http://localhost:5000/department/', {
+            method: 'GET'
         })
-			.then(res => res.json())
-			.then(data => { this.setState({list:[...this.state.list,...data.message]}); 
-                console.log(this.state.list.map(val=>val))});
-	}
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ list: [...this.state.list, ...data.message] });
+                console.log(this.state.list.map(val => val))
+            });
+    }
 
-	render() {
-        return(
+    render() {
+        return (
             <div>
-            <Navbar/>
-			<center>
-				<h2>DEPARTMENTS</h2>
-			</center> 
-            {this.state.list.map((val,index) =>(
-                <Department key={val.DeptID} name={val.name}  deptid={val.DeptID} />
-            ))}
+                <Navbar />
+                <center>
+                    <h2>DEPARTMENTS</h2>
+                </center>
+                {this.state.list.map((val, index) => (
+                    <Department key={val.DeptID} name={val.name} deptid={val.DeptID} />
+                ))}
             </div>
         );
-        
-	}
+
+    }
 }
 export default departmentview;
